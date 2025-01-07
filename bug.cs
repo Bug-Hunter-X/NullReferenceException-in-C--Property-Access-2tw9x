fd7c100e833b9bfa1c57cd610d6ca7b096b6d3c5
BugHunter-X <askzenai@gmail.com>
2025-01-07 19:06:05 +0100

    Add bug.cs file

---
+++ bug.cs
@@ -0,0 +1,15 @@
+public class ExampleClass
+{
+    public int MyProperty { get; set; }
+
+    public ExampleClass(int value)
+    {
+        MyProperty = value;
+    }
+
+    public void MyMethod()
+    {
+        // Accessing MyProperty without a null check
+        Console.WriteLine(MyProperty * 2); 
+    }
+}
